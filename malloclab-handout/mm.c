2902f50b54517796727135d931dad00af4361a3e
huibosa_macmini <huibosa@email.com>
2024-01-07 19:26:52 +0800

    feat(macro): Add macro for base point manipulation

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -41,6 +41,26 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+#define GET(p) (*(unsigned int*)(p))
+#define PUT(p, val) ((*(unsigned int*)(p)) = (val))
+
+#define PACK(size, alloc) ((size) | (alloc))
+
+#define GET_ALLOC(p) (GET(p) & 0x1)
+#define GET_SIZE(p) (GET(p) & ~0x7)
+
+#define HDRP(bp) ((char*)(bp)-WSIZE)
+#define FTRP(bp) ((char*)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp) ((char*)(bp) + GET_SIZE((char*)(bp)-WSIZE))
+#define PREV_BLKP(bp) ((char*)(bp)-GET_SIZE((char*)(bp)-DSIZE))
+
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1 << 12)
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
 /*
  * mm_init - initialize the malloc package.
  */
